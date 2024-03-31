d61566a3ba810952be75977539405123eb23bb02
Rollfire <goronos2000@gmail.com>
2024-03-31 12:38:02 +0300

    project init

---
+++ reminder-lite/reminder-lite/reminder_liteApp.swift
@@ -0,0 +1,17 @@
+//
+//  reminder_liteApp.swift
+//  reminder-lite
+//
+//  Created on 31.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct reminder_liteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
